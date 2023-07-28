3bc8011cf63b360a318eb0ea0d4bb11dadf0c5ed
Azriel <azrihell.l@gmail.com>
2023-07-28 18:41:22 -0500

    form field finished/ fixed labelName

--- src/pages/CreatePost.jsx
+++ src/pages/CreatePost.jsx
@@ -16,12 +16,48 @@ const CreatePost = () => {
   const [generatingImg, setgeneratingImg] = useState(false)
   const [loading, setloading] = useState(false)
 
+  const handleSubmit = () => {
+
+  }
+
+  const handleChange = (e) => {
+
+  }
+
+  const handleSurpriseMe = () => {
+
+  }
+
   return (
     <section className='max-w-7xl mx-auto'>
       <div>
-        <h1 className='font-extrabold  text-[#222328] text-[32px]'>Create</h1>
-        <p className='mt-2 text-[#666e75] text-[16px] max-w[500px]'>Create imaginative and visually stunning images through DALL-E AI and share them with the community!</p>
+        <h1 className='font-extrabold text-[#222328] text-[32px]'>Create</h1>
+        <p className='mt-2 text-[#666e75] text-[14px] max-w-[500px]'>Generate an imaginative image through DALL-E AI and share it with the community</p>
       </div>
+
+      <form className='mt-16 max-w-3xl' onSubmit={handleSubmit}>
+        <div className='flex flex-col gap-5'>
+          <FormField
+            labelName='Your Name'
+            type='text'
+            name='name'
+            placeholder='Ex., john doe'
+            value={form.name}
+            handleChange={handleChange}
+          />
+
+          <FormField
+            labelName='Prompt'
+            type='text'
+            name='prompt'
+            placeholder='a painting of a fox in the style of Starry Night...'
+            value={form.prompt}
+            handleChange={handleChange}
+            isSurpriseMe
+            handleSurpriseMe={handleSurpriseMe}
+          />
+        </div>
+      </form>
     </section>
   )
 }
