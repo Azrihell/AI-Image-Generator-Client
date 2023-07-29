5f0ce3be2668a30658fc13e5fc6a82403bda9daf
Azriel <azrihell.l@gmail.com>
2023-07-28 19:17:08 -0500

    generate button/img placeholder

--- src/pages/CreatePost.jsx
+++ src/pages/CreatePost.jsx
@@ -16,6 +16,10 @@ const CreatePost = () => {
   const [generatingImg, setgeneratingImg] = useState(false)
   const [loading, setloading] = useState(false)
 
+  const generateImage = () => {
+
+  }
+
   const handleSubmit = () => {
 
   }
@@ -56,7 +60,37 @@ const CreatePost = () => {
             isSurpriseMe
             handleSurpriseMe={handleSurpriseMe}
           />
+          <div className='relative bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-64 p-3 h-64 flex justify-center items-center'>
+            {form.photo ? (
+              <img
+                src={form.photo}
+                alt={form.prompt}
+                className='w-full h-full object-contain'
+              />
+            ) : (
+              <img
+                src={preview}
+                alt='preview'
+                className='w-9/12 h-9/12 object-contain opacity-40'
+              />
+            )}
+
+            {generatingImg && (
+              <div className='absolute inset-0 z-0 flex justify-center items-center bg-[rgba(0,0,0,0.5)] rounded-lg'>
+                <Loader />
+              </div>
+            )}
+          </div>
         </div>
+          <div className='mt-5 flex gap-5 '>
+              <button
+              type='button'
+              onClick={generateImage}
+              className='text-white bg-pink-400 font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center'
+              >
+                {generatingImg ? 'Generating...' : 'Generate'}
+              </button>
+          </div>
       </form>
     </section>
   )
