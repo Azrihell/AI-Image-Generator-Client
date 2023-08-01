d5fba57a9b8189c1d49a4f12a224c42bccef3927
Azriel <azrihell.l@gmail.com>
2023-08-01 16:47:01 -0500

    added functionality to the share button

--- src/pages/CreatePost.jsx
+++ src/pages/CreatePost.jsx
@@ -11,7 +11,7 @@ const CreatePost = () => {
   const [form, setForm] = useState({
     name: '',
     prompt: '',
-    photo: '',
+    photo: ''
   })
 
   const [generatingImg, setGeneratingImg] = useState(false)
@@ -42,8 +42,29 @@ const CreatePost = () => {
       alert('Please provide proper prompt')
     }
   }
-  const handleSubmit = () => {
+  const handleSubmit = async (e) => {
+    e.preventDefault()
 
+    if (form.prompt && form.photo) {
+      setLoading(true)
+
+      try {
+        const response = await fetch('http://localhost:8080/api/va/post', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(form)
+        })
+
+        await response.json()
+        navigate('/')
+      } catch (error) {
+        alert(error)
+      } finally {
+        setLoading(false)
+      }
+    }
   }
 
   const handleChange = (e) => {
